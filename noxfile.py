6ee82398900eed48fd5a5ae5a2ac75cb4acf6eff
longhaiyoushi <51020946+longhaiyoushi@users.noreply.github.com>
2026-07-12 11:52:43 +0800

    chore: Set environments

---
+++ noxfile.py
@@ -0,0 +1,28 @@
+# mypy: ignore-errors
+
+import nox
+
+
+PYTHON_VERSIONS = ['3.12', '3.13', '3.14']
+
+nox.options.default_venv_backend = 'uv'
+nox.options.sessions = ['ruff_check', 'ruff_format', 'mypy_check']
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def ruff_check(session: nox.Session):
+    session.install('ruff')
+    session.run('ruff', 'check', '.')
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def ruff_format(session: nox.Session):
+    session.install('ruff')
+    session.run('ruff', 'format', '.', '--check')
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def mypy_check(session: nox.Session):
+    session.run('uv', 'sync', '--frozen', '--no-dev')
+    session.install('mypy')
+    session.run('mypy', '.', '--strict')
